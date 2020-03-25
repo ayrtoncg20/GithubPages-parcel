58da908d629ad25cc5fd04f607f170d582d3fe7b
ayrtoncg20 <alu0100839739@ull.edu.es>
2020-03-25 00:44:17 +0000

    Creando githubpages

--- dist/js.00a46daa.js
+++ dist/js.00a46daa.js
@@ -119,7 +119,7 @@ parcelRequire = (function (modules, cache, entry, globalName) {
   return newRequire;
 })({"js/index.js":[function(require,module,exports) {
 /* index.js */
-console.log("Ready...");
+console.log('Ready...');
 },{}],"../node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
 var global = arguments[3];
 var OVERLAY_ID = '__parcel__error__overlay__';
@@ -148,7 +148,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "40083" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "41809" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
